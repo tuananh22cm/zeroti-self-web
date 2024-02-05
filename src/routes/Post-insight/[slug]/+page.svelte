5bef75d914cff208f85efdfd0dd2cf894c74dc79
Tran Tuan Anh <tuan.anh.tran@draphony.com>
2024-02-05 18:12:23 +0700

    fix dto

--- src/routes/Post-insight/[slug]/+page.svelte
+++ src/routes/Post-insight/[slug]/+page.svelte
@@ -1,5 +1,21 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
+	export let data;
+	const handleDelete = async (id:string) => {
+		try {
+			const response = await fetch(`http://localhost:3001/SponsoredPost/${id}`, {
+				method: 'DELETE',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			if (response.ok) {
+				console.log('delete successfully');
+			} else {
+				console.error('failed to delete');
+			}
+		} catch (error) {}
+	};
 </script>
 
 <h1>
@@ -12,6 +28,7 @@
 			src="https://www.facebook.com/"
 			width="500"
 			height="650"
+			title="facebook"
 			scrolling="no"
 			frameborder="0"
 			allowfullscreen="true"
@@ -20,31 +37,33 @@
 	</div>
 	<div class="post-detail">
 		<ul>
-			<li>Post Name :</li>
+			<li>Post Name :{data.data[0].name}</li>
 			<li>Like :</li>
 			<li>CMT</li>
-			<li>Category <select name="cars" id="cars">
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="opel">Opel</option>
-                <option value="audi">Audi</option>
-              </select></li>
-			<li>Assign profile <select name="cars" id="cars">
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="opel">Opel</option>
-                <option value="audi">Audi</option>
-              </select></li>
+			<li>
+				Category <select name="cars" id="cars">
+					<option value="volvo">Volvo</option>
+					<option value="saab">Saab</option>
+					<option value="opel">Opel</option>
+					<option value="audi">Audi</option>
+				</select>
+			</li>
+			<li>
+				Assign profile <select name="cars" id="cars">
+					<option value="volvo">Volvo</option>
+					<option value="saab">Saab</option>
+					<option value="opel">Opel</option>
+					<option value="audi">Audi</option>
+				</select>
+			</li>
 		</ul>
-        <div>
-            <button>Get Profile</button>
-            <button>Save</button>
-            <button>Delete Post</button>
-        </div>
+		<div>
+			<button>Get Profile</button>
+			<button>Save</button>
+			<button on:click={()=>handleDelete(data.data[0].id)}>Delete Post</button>
+		</div>
 	</div>
 </div>
 
 <style>
-
-	
 </style>
---
+++ src/routes/Post-insight/[slug]/+page.ts
@@ -0,0 +1,11 @@
+export async function load() {
+    return {
+        data:[
+            {
+            id:'659ce595e0f57b98fd4e7693',
+            name:'draphony',
+            url:"draphony.de"
+            }
+        ]
+    }
+}
